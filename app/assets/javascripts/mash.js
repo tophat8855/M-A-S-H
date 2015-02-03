a3d9544a66f7f1279f3cbb7011f3eab4d9b126d5
tophat8855 <hrfarley@gmail.com>
2015-02-03 10:37:16 -0800

    mashes are not saved until save button pressed

--- app/assets/javascripts/mash.js
+++ app/assets/javascripts/mash.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
+  var newSpouse;
+  var newKids;
+  var newVehicle;
+  var newHome;
+  var guestName;
+
   $('#game').on('click', function(event) {
     event.preventDefault();
-    var guestName = $('#guest').val();
+    guestName = $('#guest').val();
 
-    var newHome;
     var randHome = Math.random();
     if (randHome < 0.25) {
       newHome = 'mansion';
@@ -16,28 +21,43 @@ $(document).ready(function() {
     }
 
     var randSpouse = Math.ceil(Math.random() * 4);
-    var newSpouse = $('#spouse' + randSpouse).val();
+    newSpouse = $('#spouse' + randSpouse).val();
 
     var randKid = Math.ceil(Math.random() * 4);
-    var newKids = $('#kids' + randKid).val();
+    newKids = $('#kids' + randKid).val();
 
     var randVehicle = Math.ceil(Math.random() * 4);
-    var newVehicle = $('#vehicle' + randVehicle).val();
+    newVehicle = $('#vehicle' + randVehicle).val();
 
+    var results = '<p>' + guestName + ', you will marry ' + newSpouse + ' and live in a fabulous ' + newHome + '! Have fun cruising the town in your ' + newVehicle + ' with ' + newKids + ' kids!</p>';
+    $('body').append('<div id="results">' + results + '</div>');
+    $('body').append('<div class="ui buttons save">\
+    <div id="again" class="ui button">Start Over</div>\
+    <div class="or"></div>\
+    <div id="save" class="ui positive button">Save</div>\
+    </div>');
+
+  });
+
+  $('body').on('click', '#again', function(event) {
+    $("#results").remove();
+    $(".save").remove();
+    $('#new_mash')[0].reset();
+  });
+
+  $('body').on('click', '#save', function(event){
     $.ajax('/mashes',
     {type: 'post',
-    data: {
-      mash: {
-        guest: guestName,
-        home: newHome,
-        spouse: newSpouse,
-        kids: newKids,
-        vehicle: newVehicle,
+      data: {
+        mash: {
+          guest: guestName,
+          home: newHome,
+          spouse: newSpouse,
+          kids: newKids,
+          vehicle: newVehicle,
+        }
       }
-    }
-  }).done(function(data) {
-    var results = '<p>' + data.guest + ', you will marry ' + data.spouse + ' and live in a fabulous ' + data.home + '! Have fun cruising the town in your ' + data.vehicle + ' with ' + data.kids + ' kids!</p>';
-    $('body').append(results);
-  });
+    }).done(function(data) {
+    });
   });
 });
