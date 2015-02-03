3b0f31dc081f151068c5a724385ba7277f7f7871
tophat8855 <hrfarley@gmail.com>
2015-02-02 17:20:22 -0800

    set up semantic ui

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'spring',        group: :development
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'semantic-ui-sass', '~> 1.8.1.0'
+
 # Use unicorn as the app server
 # gem 'unicorn'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :parties
   has_many :guests, through: :parties
-  validates :email, prescence: true, uniqueness: true, confirmation: true
+  validates :email, presence: true, uniqueness: true, confirmation: true
 end
